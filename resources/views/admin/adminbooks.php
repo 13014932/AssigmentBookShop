0936bad4503f1b23166d1d7e3bd881b7e276d60d
satpal khatana <ersatpal127@gmail.com>
2018-05-07 13:19:53 +0530

    Assigment-BookShop
    Here i make fresh files for my assigment BookShop.

---
+++ resources/views/admin/adminbooks.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lenovo
+ * Date: 07-05-2018
+ * Time: 01:18 PM
+ */
---
+++ resources/views/datatable/DataTableLib.php
@@ -0,0 +1,13 @@
+
+<!---  DataTable Lib -->
+<!-- Below files are use for  datatable-->
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+
+<link href="https://cdn.datatables.net/buttons/1.5.1/css/buttons.dataTables.min.css" rel="stylesheet" type="text/css">
+<link href="https://cdn.datatables.net/select/1.2.5/css/select.dataTables.min.css" rel="stylesheet" type="text/css">
+
+<script src="https://cdn.datatables.net/buttons/1.5.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/select/1.2.5/js/dataTables.select.min.js"></script>
+
+
+
