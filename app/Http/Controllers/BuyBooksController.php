1d8d8457733d90e0ff3f5490c68b392a6106980c
Satpal Khatana <13014932@users.noreply.github.com>
2018-05-08 23:57:08 +0530

    Update BuyBooksController v_0.1

--- app/Http/Controllers/BuyBooksController.php
+++ app/Http/Controllers/BuyBooksController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 use App\Lib\BookShopLib;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 
@@ -15,15 +14,14 @@ class BuyBooksController extends Controller
         try {
             $data = new BookShopLib();
             $store = $data->storeBookAfterBuy($request);
-
-            if ($store) {
-                return redirect('/buydbooks');
-            }
+            
+            return redirect('/buydbooks');
+            
         }
         catch (\Exception $e) {
-            Log::error($e->getMessage() . " => on file " . $e->getFile() . " => on line number = " . $e->getLine());
+           return ($e->getMessage() . " => on file " . $e->getFile() . " => on line number = " . $e->getLine());
 
-            return false;
+          
         }
 
     }
@@ -36,13 +34,12 @@ class BuyBooksController extends Controller
             $data = new BookShopLib();
             $data = $data->viewBooksAfterBuy();
 
-            if ($data) {
-                return view('Buydbooks', ['buydbooks' => $data]);
-            }
+             return view('Buydbooks', ['buydbooks' => $data]);
+           
         } catch (\Exception $e) {
-            Log::error($e->getMessage() . " => on file " . $e->getFile() . " => on line number = " . $e->getLine());
+            return ($e->getMessage() . " => on file " . $e->getFile() . " => on line number = " . $e->getLine());
 
-            return false;
+            
         }
 
 
