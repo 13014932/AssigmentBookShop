6b2be009b6df1a4445117f4ed11e1cdadd364d88
satpal khatana <ersatpal127@gmail.com>
2018-05-23 20:13:37 +0530

    update test BookTest V_0_23_810

--- tests/Feature/BookTest.php
+++ tests/Feature/BookTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+
 use Tests\TestCase;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -9,52 +10,99 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Lib\Crud\Book as BookLib;
 use Illuminate\Support\Facades\Validator;
 
+class MyException extends \Exception
+{
+    public function errorMessage()
+    {
+        //error message
+
+        $errorMsg = ' Error => ' . $this->getMessage() ;
+        return $errorMsg;
+    }
+}
 class BookTest extends TestCase
+
 {
     /**
      * A basic test example.
      *
      * @return void
      */
+
+    //multiple catches
     //method to test NEW BOOK
     public function testCreateBook()
     {
 
+        $book_data = ['name' => 'varun', 'price' => 1, 'quantity' => 1];
 
         try {
 
-            $book_name = 'varun';
-            $book_data = ['name' => $book_name, 'price' => 1, 'quantity' => 1];
+            if(strlen($book_data['name']) > 8) {
 
-//            $this->assertLessThanOrEqual(8, strlen($book_name));
-
-            $validator = Validator::make($book_data, [
-                'name' => 'required|max:8',
-                'price' => 'required|integer|min:0',
-                'quantity' => 'required|integer|min:0',
-
-            ]);
-
-            if ($validator->fails()) {
-                $error = \Illuminate\Validation\ValidationException::withMessages([
-                    'name' => ['Name must be required and should not exceed the limit of eight'],
-                    'price' => ['The price must be required and not be negative'],
-                    'quantity' => ['Quantity should be required and should not be negative'],
-                ]);
-                throw $error;
+                throw new MyException("The Book name may not be greater than 8 characters");
             }
 
+            if(is_int ($book_data['price'] ) == false ) {
+                throw new MyException("The price must be at least 1");
+            }
+            if(is_int ($book_data['quantity'] ) == false ) {
+                throw new MyException("The quantity must be at least 1");
+            }
             BookLib::createBook($book_data);
-
-
-        } catch (\Exception $error) {
-
-            log::error($error->getMessage() . " => on file " . $error->getFile() . " => on line number = " . $error->getLine());
-
         }
+
+        catch (MyException $e) {
+            echo $e->errorMessage();
+        }
+
+        catch(MyException $e) {
+            echo $e->getMessage();
+        }
+        catch(\Exception $e) {
+            echo $e->getMessage();
+        }
+
+        /*  below code  with laravel validation (commented)*/
+
+//        try {
+//
+////            $book_name = 'varun';
+//            $book_data = ['name' => 'varun', 'price' => 1, 'quantity' => 1];
+//
+////    *comment        $this->assertLessThanOrEqual(8, strlen($book_name));
+//
+//            $validator = Validator::make($book_data, [
+//                'name' => 'required|max:8',
+//                'price' => 'required|integer|min:1',
+//                'quantity' => 'required|integer|min:1',
+//
+//            ]);
+//
+//            if ($validator->fails()) {
+//
+//                $errors = $validator->errors()->all();
+//
+////        *comment       log::info(implode(" ",$errors)) ;
+////
+////         *comment      $this->fail( "error 101" );
+//                throw new \Exception (implode(" ", $errors));
+//            }
+//
+//            BookLib::createBook($book_data);
+//
+//
+//        } catch (\Exception $e) {
+//
+////       *comment    $this->assertEquals( "error in validate", $e->getMessage());
+////        *comment   log::error($e->getMessage());
+//            echo $e->getMessage();
+//
+//        }
 
 
     }
+
 
 //method to test update BOOK
     public function testUpdateBook()
