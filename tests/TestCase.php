36055e8be6d633830a20988335dd0d6908627a0a
Satpal Khatana <ersatpal127@gmail.com>
2018-07-26 18:33:30 +0530

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,3 +8,15 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 }
+// Something I ran into a while back while writing tests.
+
+// If intentionally testing code for an exception then it is required to add an annotation above the test method.
+
+
+// For example if the code will throw IllegalArgumentException.
+/**
+* @expectedException IllegalArgumentException
+*/
+public function testMethodThrowsException() {
+    // Code that will throw IllegalArgumentException
+}
